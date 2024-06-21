b48e7e5e0ae37507481e06a7bc9fd263b08c95cd
Yigit Sadic <yigitsadic@gmail.com>
2024-06-21 23:26:37 +0300

    Added astronvim community plugins

---
+++ community.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.comment.mini-comment" },
+  { import = "astrocommunity.note-taking.global-note-nvim" },
+  { import = "astrocommunity.git.blame-nvim" },
+}
